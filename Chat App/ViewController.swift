25ae892d4ab47184f09127b46864a603aa19cdeb
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-08-23 15:51:54 -0400

    Initial Commit

---
+++ Chat App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat App
+//
+//  Created by Justin Wozniak on 2019-08-23.
+//  Copyright © 2019 Justin Wozniak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
